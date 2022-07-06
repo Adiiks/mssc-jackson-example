301191983d5ddaceec0658419426057b09f49af8
Adiks <apacholak97@interia.pl>
2022-07-06 09:55:25 +0200

    custom Deserializer with Jackson

--- src/test/java/com/adrian/msscjacksonexample/model/BeerDTOTest.java
+++ src/test/java/com/adrian/msscjacksonexample/model/BeerDTOTest.java
@@ -18,10 +18,10 @@ class BeerDTOTest extends BaseTest {
 
     @Test
     void testDeserializeDTO() throws JsonProcessingException {
-        String json = "{\"beerId\":\"58288889-296d-4374-9ffe-d08290b73df9\",\"beerName\":\"BeerName\"," +
-                "\"beerStyle\":\"Ale\",\"upc\":13213131,\"price\":12.45," +
-                "\"createdDate\":\"2022-07-05T11:21:44.4023278+02:00\"," +
-                "\"lastUpdatedDate\":\"2022-07-05T11:21:44.4023278+02:00\"}\n";
+        String json = "{\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":13213131,\"price\":\"12.45\"," +
+                "\"createdDate\":\"2022-07-06T09:52:38+0200\"," +
+                "\"lastUpdatedDate\":\"2022-07-06T09:52:38.2290454+02:00\",\"myLocalDate\":\"20220706\"," +
+                "\"beerId\":\"ae3f7d01-29c9-4495-9d30-2d727ec983c0\"}\n";
 
         BeerDTO beerDTO = objectMapper.readValue(json, BeerDTO.class);
 
